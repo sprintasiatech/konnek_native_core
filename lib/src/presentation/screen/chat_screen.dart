b4f50edb70223673e3882fc34c9e7e43c65a1580
Fauzan Akmal Mahdi <fauzan.akmal@sprintasia.co.id>
2025-05-09 15:46:53 +0700

    update: fix clear room closed

--- lib/src/presentation/screen/chat_screen.dart
+++ lib/src/presentation/screen/chat_screen.dart
@@ -86,7 +86,7 @@ class _ChatScreenState extends State<ChatScreen> {
           if (mounted) {
             setState(() {});
           }
-          AppController.clearRoomClosed();
+          // AppController.clearRoomClosed();
           // disconnectSocket();
         };
         AppController.onSocketCSATCalled = () {
