4e11cabb30a9efb5ad196bc9db61175edbacbe20
Fauzan Akmal Mahdi <fauzan.akmal@sprintasia.co.id>
2025-05-15 16:40:21 +0700

    update: fix loading doesn't hiding

--- lib/src/presentation/screen/chat_screen.dart
+++ lib/src/presentation/screen/chat_screen.dart
@@ -33,13 +33,13 @@ class _ChatScreenState extends State<ChatScreen> {
   final ValueNotifier<bool> _showToggleButton = ValueNotifier(false);
 
   void scrollListenerFunc() {
-    AppLoggerCS.debugLog("Scroll position: ${_scrollController.position.pixels}");
+    // AppLoggerCS.debugLog("Scroll position: ${_scrollController.position.pixels}");
     if (_scrollController.position.pixels > 100.0) {
       _showToggleButton.value = true;
     } else {
       _showToggleButton.value = false;
     }
-    AppLoggerCS.debugLog("_showToggleButton.value: ${_showToggleButton.value}");
+    // AppLoggerCS.debugLog("_showToggleButton.value: ${_showToggleButton.value}");
   }
 
   @override
@@ -243,6 +243,9 @@ class _ChatScreenState extends State<ChatScreen> {
                           scrollController: _scrollController,
                           enablePullUp: true,
                           enablePullDown: false,
+                          footer: const ClassicFooter(
+                            loadStyle: LoadStyle.ShowWhenLoading,
+                          ),
                           onLoading: () async {
                             await AppController().loadMoreConversation(
                               onSuccess: () {
