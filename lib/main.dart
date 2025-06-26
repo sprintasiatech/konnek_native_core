2ac0081b5892999b2a9697e4437390db3486657f
Fauzan Akmal Mahdi <fauzan.akmal@sprintasia.co.id>
2025-06-26 14:33:36 +0700

    update: print error log

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,23 @@
+import 'dart:developer';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:konnek_native_core/app.dart';
 import 'package:konnek_native_core/inter_module.dart';
 
 void main() async {
+  FlutterError.onError = (details) {
+    FlutterError.presentError(details);
+    log("[main][FlutterError.onError]: $details");
+    debugPrint("[main][FlutterError.onError]: $details");
+  };
+  PlatformDispatcher.instance.onError = (error, stack) {
+    log("[main][FlutterError.onError] error: $error");
+    debugPrint("[main][FlutterError.onError] error: $error");
+    log("[main][FlutterError.onError] stack: $stack");
+    debugPrint("[main][FlutterError.onError] stack: $stack");
+    return true;
+  };
   runApp(const MyApp());
   await InterModule().initialize();
 }
